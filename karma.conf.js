9b7ea19091216b30f126c5868f5bb1c12dac9f27
arif <a.arif.r@gmail.com>
2017-10-07 06:52:07 +0400

    ci(Fix karma-coverage configuration): Fix karma-coverage configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,20 @@
 module.exports = function(config) {
     config.set({
         frameworks: ['mocha', 'chai'],
-        files: ['lib/yalla.js','test/**/*.js'],
-        reporters: ['progress'],
+        files: ['src/yalla.js','test/**/*.js'],
+        reporters: ['progress','coverage'],
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            'src/**/*.js': ['coverage']
+            //'src/yalla.js': ['coverage']
+        },
+        // optionally, configure the reporter
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
+        },
         port: 9876,  // karma web server port
         colors: true,
         logLevel: config.LOG_INFO,
