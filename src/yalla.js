0de437390e02a8a2caa4a4e1da26c08fb19f78d1
arif <a.arif.r@gmail.com>
2017-10-17 21:07:56 +0400

    perf($compile): Improve performance by improving loop
    Improve performance by improving loop

--- src/yalla.js
+++ src/yalla.js
@@ -1,166 +1,189 @@
+/*
+ Copyright (c) 2017 Arif Rachim
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 const PLACEHOLDER_CONTENT = '∞';
 const PLACEHOLDER = `<!--${PLACEHOLDER_CONTENT}-->`;
-
 const isMinimizationAttribute = node => {
     return ['checked', 'compact', 'declare', 'defer', 'disabled', 'ismap',
             'noresize', 'noshade', 'nowrap', 'selected'].indexOf(node.nodeName) >= 0;
 };
 
-function render(val,node){
-    if(!node.$content){
+
+function render(val, node) {
+
+    if (!node.$content) {
         let placeHolder = document.createComment(PLACEHOLDER_CONTENT);
         node.appendChild(placeHolder);
-        _render(val,placeHolder);
+        _render(val, placeHolder);
         node.$content = placeHolder;
-    }else{
-        _render(val,node.$content);
+    } else {
+        _render(val, node.$content);
     }
 }
 
-function destroy(val){
-    if(val instanceof HtmlTemplate){
+function destroy(val) {
+    if (val instanceof HtmlTemplate) {
         val.destroy();
-    }else if(val instanceof HtmlTemplateCollection){
+    } else if (val instanceof HtmlTemplateCollection) {
         val.destroy();
-    }else if(val.parentNode){
+    } else if (val.parentNode) {
         val.parentNode.removeChild(val);
     }
 }
 
-function _render(val, node){
-    if(val instanceof HtmlTemplate){
-        _renderHtmlTemplate(val,node);
-    }else if(val instanceof HtmlTemplateCollection){
-        _renderHtmlTemplateCollection(val,node);
-    }else{
-        _renderText(val,node);
+function _render(val, node) {
+    if (val instanceof HtmlTemplate) {
+        _renderHtmlTemplate(val, node);
+    } else if (val instanceof HtmlTemplateCollection) {
+        _renderHtmlTemplateCollection(val, node);
+    } else {
+        _renderText(val, node);
     }
 }
 
-function getFirstNodeFromTemplate(template,placeHolder) {
-    if(template instanceof HtmlTemplate){
+function getFirstNodeFromTemplate(template, placeHolder) {
+    if (template instanceof HtmlTemplate) {
         return template.nodeTree[0];
-    }else if(template instanceof HtmlTemplateCollection){
-        return getFirstNodeFromTemplate(template.htmlTemplates[template.keys[0]],placeHolder);
-    }else{
+    } else if (template instanceof HtmlTemplateCollection) {
+        return getFirstNodeFromTemplate(template.htmlTemplates[template.keys[0]], placeHolder);
+    } else {
         return placeHolder.$content;
     }
 }
-function _renderHtmlTemplateCollection(newTemplateCollection, newPlaceHolder){
+function _renderHtmlTemplateCollection(newTemplateCollection, newPlaceHolder) {
 
     let placeHolders = {};
     let oldPlaceHolders = {};
     let oldHtmlTemplateCollection = newPlaceHolder.$content;
 
-    if(oldHtmlTemplateCollection && (!(oldHtmlTemplateCollection instanceof HtmlTemplateCollection))){
+    if (oldHtmlTemplateCollection && (!(oldHtmlTemplateCollection instanceof HtmlTemplateCollection))) {
         destroy(oldHtmlTemplateCollection);
         oldHtmlTemplateCollection = null;
     }
 
-    if(oldHtmlTemplateCollection){
+    if (oldHtmlTemplateCollection) {
         oldPlaceHolders = oldHtmlTemplateCollection.placeHolders;
         oldHtmlTemplateCollection.keys.forEach(oldKey => {
-            if(newTemplateCollection.keys.indexOf(oldKey) < 0){
+            if (newTemplateCollection.keys.indexOf(oldKey) < 0) {
                 let oldPlaceHolder = oldPlaceHolders[oldKey];
                 removePlaceholder(oldPlaceHolder);
             }
         });
     }
 
-    newTemplateCollection.keys.reduceRight((prev,key,index,array) => {
+    newTemplateCollection.keys.reduceRight((prev, key) => {
         let htmlTemplate = newTemplateCollection.htmlTemplates[key];
 
         let placeHolder = document.createComment(PLACEHOLDER_CONTENT);
-        if(oldPlaceHolders[key]){
+        if (oldPlaceHolders[key]) {
             placeHolder = oldPlaceHolders[key];
         }
-        if(placeHolder.nextSibling){
-            if(placeHolder.nextSibling != prev){
-                newPlaceHolder.parentNode.insertBefore(placeHolder,prev);
-            }
-        }else{
-            newPlaceHolder.parentNode.insertBefore(placeHolder,prev);
+
+        if( (!placeHolder.nextSibling) || (placeHolder.nextSibling != prev) ){
+            newPlaceHolder.parentNode.insertBefore(placeHolder, prev);
         }
+
         placeHolders[key] = placeHolder;
-        if(oldPlaceHolders[key]){
+        if (oldPlaceHolders[key]) {
             let oldHtmlTemplate = oldHtmlTemplateCollection.htmlTemplates[key];
-            _render(htmlTemplate,placeHolder);
-            if(oldHtmlTemplate instanceof HtmlTemplate && htmlTemplate instanceof HtmlTemplate && oldHtmlTemplate.key == htmlTemplate.key){
+            _render(htmlTemplate, placeHolder);
+            if (oldHtmlTemplate instanceof HtmlTemplate && htmlTemplate instanceof HtmlTemplate && oldHtmlTemplate.key == htmlTemplate.key) {
                 newTemplateCollection.htmlTemplates[key] = oldHtmlTemplate;
             }
-        }else{
-            _render(htmlTemplate,placeHolder);
+        } else {
+            _render(htmlTemplate, placeHolder);
         }
-        return getFirstNodeFromTemplate(newTemplateCollection.htmlTemplates[key],placeHolder);
+        return getFirstNodeFromTemplate(newTemplateCollection.htmlTemplates[key], placeHolder);
 
-    },newPlaceHolder);
+    }, newPlaceHolder);
     newTemplateCollection.placeHolders = placeHolders;
     newPlaceHolder.$content = newTemplateCollection;
 }
 
-function removePlaceholder(placeHolder){
-    if(placeHolder.$content instanceof HtmlTemplate){
+function removePlaceholder(placeHolder) {
+    if (placeHolder.$content instanceof HtmlTemplate) {
         placeHolder.$content.nodeTree.forEach(n => n.parentNode.removeChild(n));
-    }else if(Array.isArray(placeHolder.$content)) {
+    } else if (Array.isArray(placeHolder.$content)) {
         placeHolder.$content.forEach(pc => removePlaceholder(pc));
     }
     placeHolder.parentNode.removeChild(placeHolder);
 }
 
+function _buildTemplate(htmlTemplate, node) {
+    htmlTemplate.generateNodeTree().forEach(n => node.parentNode.insertBefore(n, node));
+    node.$content = htmlTemplate;
+}
 function _renderHtmlTemplate(htmlTemplate, node) {
-    if(node.$content){
-        if(node.$content instanceof  HtmlTemplate){
+    if (node.$content) {
+        if (node.$content instanceof HtmlTemplate) {
             node.$content.applyValues(htmlTemplate.values);
-            node.$content.nodeTree.reduceRight(function(next,item,index){
-                if(item.nextSibling && item.nextSibling != next && next.parentNode){
-                    next.parentNode.insertBefore(item,next);
+            node.$content.nodeTree.reduceRight(function (next, item) {
+                if (item.nextSibling && item.nextSibling != next && next.parentNode) {
+                    next.parentNode.insertBefore(item, next);
                 }
                 return item;
-            },node);
-        }else {
+            }, node);
+        } else {
             destroy(node.$content);
-            htmlTemplate.generateNodeTree().forEach(n => node.parentNode.insertBefore(n,node));
-            node.$content = htmlTemplate;
+            _buildTemplate(htmlTemplate, node);
         }
-    }else{
-        htmlTemplate.generateNodeTree().forEach(n => node.parentNode.insertBefore(n,node));
-        node.$content = htmlTemplate;
+    } else {
+        _buildTemplate(htmlTemplate, node);
     }
 }
 
-function _renderText(val, node){
-    if(node.parentNode == null){
+function _renderText(val, node) {
+    if (node.parentNode == null) {
         return;
     }
-    if(node.$content){
+    if (node.$content) {
         destroy(node.$content);
     }
     let textNode = document.createTextNode(val);
-    node.parentNode.insertBefore(textNode,node);
+    node.parentNode.insertBefore(textNode, node);
     node.$content = textNode;
 }
 
-function html(strings,...values){
-    let key = strings.join('').replace(/\s/g,'');
-    return new HtmlTemplate(strings,values,key);
+function html(strings, ...values) {
+    let key = strings.join('').replace(/\s/g, '');
+    return new HtmlTemplate(strings, values, key);
 }
 
-function cache(key){
+function cache(key) {
     return {
-        html : function(strings,...values){
-            return new HtmlTemplate(strings,values,key);
+        html: function (strings, ...values) {
+            return new HtmlTemplate(strings, values, key);
         }
     }
 }
 
-function htmlCollection(items, keyFn, templateFn){
-    return new HtmlTemplateCollection(items,keyFn,templateFn);
+function htmlCollection(items, keyFn, templateFn) {
+    return new HtmlTemplateCollection(items, keyFn, templateFn);
 }
 
 const _cache = {};
 
-class HtmlTemplateCollection{
-    constructor(items,keyFn,templateFn){
+class HtmlTemplateCollection {
+    constructor(items, keyFn, templateFn) {
         this.items = items;
         this.keyFn = typeof keyFn === 'function' ? keyFn : (i) => i[keyFn];
         this.templateFn = templateFn;
@@ -169,22 +192,21 @@ class HtmlTemplateCollection{
         this._init();
     }
 
-    _init(){
+    _init() {
         let self = this;
         let index = self.items.length;
-        while(index--){
+        while (index--) {
             let item = self.items[index];
-            let key = self.keyFn.apply(self,[item]);
-            let htmlTemplate = self.templateFn.apply(self,[item,index,self.items]);
-            self.htmlTemplates[key] = htmlTemplate;
+            let key = self.keyFn.apply(self, [item]);
+            self.htmlTemplates[key] = self.templateFn.apply(self, [item, index, self.items]);
             self.keys.push(key);
         }
         self.keys.reverse();
     }
 
-    destroy(){
+    destroy() {
         this.keys.forEach(key => {
-            if(this.htmlTemplates[key] instanceof HtmlTemplate){
+            if (this.htmlTemplates[key] instanceof HtmlTemplate) {
                 this.htmlTemplates[key].destroy();
                 this.placeHolders[key].parentNode.removeChild(this.placeHolders[key]);
             }
@@ -201,37 +223,37 @@ class HtmlTemplateCollection{
 function getPath(node) {
     var i = 0;
     let child = node;
-    while ((child = child.previousSibling) != null){
+    while ((child = child.previousSibling) != null) {
         i++;
     }
     let path = [];
     path.push(i);
-    if(node.parentNode && node.parentNode.nodeType != Node.DOCUMENT_FRAGMENT_NODE){
+    if (node.parentNode && node.parentNode.nodeType != Node.DOCUMENT_FRAGMENT_NODE) {
         return path.concat(getPath(node.parentNode));
     }
     return path;
 }
 
-class HtmlTemplate{
-    constructor(strings,values,key){
+class HtmlTemplate {
+    constructor(strings, values, key) {
         this.strings = strings;
         this.values = values;
         this.key = key;
     }
 
-    generateNodeTree(){
+    generateNodeTree() {
         let key = this.key;
-        if(!_cache[key]){
+        if (!_cache[key]) {
             let template = document.createElement('template');
             template.innerHTML = this.strings.join(PLACEHOLDER);
             _cache[key] = template;
         }
         let template = _cache[key];
         this.content = template.content.cloneNode(true);
-        if(!template.dynamicNodesPath){
+        if (!template.dynamicNodesPath) {
             this._coldStart();
             template.dynamicNodesPath = this.dynamicNodesPath;
-        }else{
+        } else {
             this.dynamicNodesPath = template.dynamicNodesPath;
             this._warmStart();
         }
@@ -240,10 +262,10 @@ class HtmlTemplate{
         return this.nodeTree;
     }
 
-    _coldStart(){
+    _coldStart() {
         let results = [];
         let resultsPath = [];
-        this._lookDynamicNodes(Array.from(this.content.childNodes),results,resultsPath);
+        this._lookDynamicNodes(Array.from(this.content.childNodes), results, resultsPath);
         this.dynamicNodes = results;
         this.dynamicNodesPath = resultsPath.map(path => {
             return path.reverse();
@@ -251,32 +273,32 @@ class HtmlTemplate{
         this.applyValues(this.values);
     }
 
-    _warmStart(){
+    _warmStart() {
         this._lookDynamicNodesFromPath();
         this.applyValues(this.values);
     }
 
-    _lookDynamicNodesFromPath(){
-        let dynamicNodes = this.dynamicNodesPath.map((path) =>{
-            return path.reduce(function(content,path){
-                if(typeof path == 'number'){
+    _lookDynamicNodesFromPath() {
+        this.dynamicNodes = this.dynamicNodesPath.map((path) => {
+            return path.reduce(function (content, path) {
+                if (typeof path == 'number') {
                     return content.childNodes[path];
-                }else{
+                } else {
                     let attribute = content.attributes[path.name];
                     attribute.$dynamicAttributeLength = path.dynamicLength;
                     attribute.$dynamicAttributeLengthPos = 0;
                     return attribute;
                 }
 
-            },this.content)
+            }, this.content)
         });
 
-        this.dynamicNodes = dynamicNodes;
+
     }
 
-    _lookDynamicNodes(childNodes, results,resultsPath) {
+    _lookDynamicNodes(childNodes, results, resultsPath) {
         childNodes.forEach(node => {
-            if(node instanceof Comment && node.nodeValue == PLACEHOLDER_CONTENT){
+            if (node instanceof Comment && node.nodeValue == PLACEHOLDER_CONTENT) {
                 results.push(node);
                 resultsPath.push(getPath(node));
             }
@@ -284,32 +306,35 @@ class HtmlTemplate{
                 Array.from(node.attributes).reduce((results, attribute) => {
                     if (attribute.nodeValue.indexOf(PLACEHOLDER) >= 0) {
                         let dynamicLength = attribute.nodeValue.split(PLACEHOLDER).length - 1;
-                        for(let i = 0;i<dynamicLength;i++){
+                        for (let i = 0; i < dynamicLength; i++) {
                             attribute.$dynamicAttributeLength = dynamicLength;
                             attribute.$dynamicAttributeLengthPos = 0;
                             results.push(attribute);
-                            let path = [{name:attribute.nodeName,dynamicLength:dynamicLength}].concat(getPath(attribute.ownerElement));
+                            let path = [{
+                                name: attribute.nodeName,
+                                dynamicLength: dynamicLength
+                            }].concat(getPath(attribute.ownerElement));
                             resultsPath.push(path);
                         }
                     }
                     return results;
                 }, results);
-                this._lookDynamicNodes(Array.from(node.childNodes), results,resultsPath);
+                this._lookDynamicNodes(Array.from(node.childNodes), results, resultsPath);
             }
         });
     }
 
-    applyValues(values){
-        this.dynamicNodes.forEach((dn,index) => {
+    applyValues(values) {
+        this.dynamicNodes.forEach((dn, index) => {
             if (dn.nodeType === Node.ATTRIBUTE_NODE) {
                 HtmlTemplate._applyAttributeNode(dn, values[index]);
             } else {
-                _render(values[index],dn);
+                _render(values[index], dn);
             }
         });
     }
 
-    destroy(){
+    destroy() {
         this.nodeTree.forEach(n => n.parentNode.removeChild(n));
         this.content = null;
     }
@@ -322,10 +347,10 @@ class HtmlTemplate{
             if (!node.$valueOriginal) {
                 node.$valueOriginal = node.value;
             }
-            if(node.$dynamicAttributeLengthPos == node.$dynamicAttributeLength){
+            if (node.$dynamicAttributeLengthPos == node.$dynamicAttributeLength) {
                 node.$dynamicAttributeLengthPos = 0;
             }
-            if(node.$dynamicAttributeLengthPos == 0) {
+            if (node.$dynamicAttributeLengthPos == 0) {
                 node.$value = node.$valueOriginal;
             }
             if (isMinimizationAttribute(node)) {
@@ -335,7 +360,7 @@ class HtmlTemplate{
                     return index == 0 ? data : `${result}${index == 1 ? value : PLACEHOLDER}${data}`;
                 }, '');
                 node.$dynamicAttributeLengthPos++;
-                if(node.$dynamicAttributeLengthPos == node.$dynamicAttributeLength && node.value != node.$value){
+                if (node.$dynamicAttributeLengthPos == node.$dynamicAttributeLength && node.value != node.$value) {
                     node.value = node.$value;
                 }
             }
